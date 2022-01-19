5fa21e3eb65d585c0a6d3935c934b5ce46dbf46d
Kiên Trung <phankien209@gmail.com>
2022-01-19 21:22:12 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const menuBar = document.querySelector(".menu-bar")
+menuBar.addEventListener("click",function(){
+    menuBar.classList.toggle("active")
+    document.querySelector(".menu-items").classList.toggle("active")
+})
+window.addEventListener("scroll",function(){
+    const x = this.pageYOffset;
+    if (x>80) {document.querySelector(".top").classList.add("active")}
+    else {document.querySelector(".top").classList.remove("active")}
+})
+const menuTitle = document.querySelector(".menu-title");
+menuTitle.addEventListener("click",function(x){
+    if(x.target.classList.contains("menu-button")){
+        const Target = x.target.getAttribute("data-title");
+        menuTitle.querySelector(".active").classList.remove("active")
+        x.target.classList.add("active");
+
+        const menuItem = document.querySelector(".menu");
+        menuItem.querySelector(".menu-item-content.active").classList.remove("active");
+        menuItem.querySelector(Target).classList.add("active")
+    }
+})
